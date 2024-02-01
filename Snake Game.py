fb5970625d6ab9d4280ef08b58e7b4eab1c06914
Richard Solomon <35183675+RichieSolo@users.noreply.github.com>
2024-01-31 18:52:39 -0600

    Snake Class Implementation
    Implemented a class into the main game script.

--- Snake Game.py
+++ Snake Game.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import math
+from snake import Snake  # assuming snake.py is in the same directory
 
 # Constants
 WIDTH = 640
@@ -10,12 +11,8 @@ SQUARES = int(WIDTH / PIXELS)
 BG1 = (156, 210, 54)
 BG2 = (147, 203, 57)
 
-# Player
-player_pos_x = WIDTH / 2
-player_pos_y = HEIGHT / 2
-player_vel_x = 0 
-player_vel_y = 0 
-movement_speed = 200
+# Snake 
+snake = Snake("Player 1")
 
 # Food
 food_pos_x = random.randrange(0, WIDTH - PIXELS, 32)
@@ -50,61 +47,76 @@ def draw_scoreboard(screen, score_value, x, y):
 
 def spawn_food(screen, x, y):
     pygame.draw.rect(screen, "red", [(x, y), (PIXELS, PIXELS)])
-    
+
+def get_food_pos(snake_body):
+    while True:
+        x = random.randint(0, WIDTH - PIXELS)
+        y = random.randint(0, WIDTH - PIXELS)
+        if [x, y] not in snake_body:
+            return x, y
 
 # Game Program Loop
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-            
+    
+    #Draw the background     
     draw_background(screen)
     
-    player_head = pygame.draw.rect(screen, "black", [(player_pos_x, player_pos_y), (PIXELS, PIXELS)])
+    #Head of snake drawn
+    snake.snake_draw(PIXELS, screen)
     
     #Player Movement
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_vel_x = 0
-        player_vel_y = -movement_speed * dt 
-    if keys[pygame.K_s]:
-        player_vel_x = 0
-        player_vel_y = movement_speed * dt 
-    if keys[pygame.K_a]:
-        player_vel_y = 0
-        player_vel_x = -movement_speed * dt 
-    if keys[pygame.K_d]:
-        player_vel_y = 0
-        player_vel_x = movement_speed * dt 
-
-    player_pos_x += player_vel_x 
-    player_pos_y += player_vel_y 
+    if keys[pygame.K_w] and snake.current_direction != "s":
+        snake.player_vel_x = 0
+        snake.player_vel_y = -snake.movement_speed * dt
+        snake.current_direction = "w"
+    if keys[pygame.K_s] and snake.current_direction != "w":
+        snake.player_vel_x = 0
+        snake.player_vel_y = snake.movement_speed * dt
+        snake.current_direction = "s"
+    if keys[pygame.K_a] and snake.current_direction != "d":
+        snake.player_vel_y = 0
+        snake.player_vel_x = -snake.movement_speed * dt
+        snake.current_direction = "a"
+    if keys[pygame.K_d] and snake.current_direction != "a":
+        snake.player_vel_y = 0
+        snake.player_vel_x = snake.movement_speed * dt
+        snake.current_direction = "d"
+        
+    snake.player_pos_x += snake.player_vel_x 
+    snake.player_pos_y += snake.player_vel_y 
+    
+    snake.snake_update()
     
     #Boundaries
-    if player_pos_x <= 0:
-        player_pos_x = 0
-    elif player_pos_x >= WIDTH - PIXELS:
-        player_pos_x = WIDTH - PIXELS
+    if snake.player_pos_x <= 0:
+        snake.player_pos_x = 0
+    elif snake.player_pos_x >= WIDTH - PIXELS:
+        snake.player_pos_x = WIDTH - PIXELS
 
-    if player_pos_y <= 0:
-        player_pos_y = 0
-    elif player_pos_y >= HEIGHT - PIXELS:
-        player_pos_y = HEIGHT - PIXELS
+    if snake.player_pos_y <= 0:
+        snake.player_pos_y = 0
+    elif snake.player_pos_y >= HEIGHT - PIXELS:
+        snake.player_pos_y = HEIGHT - PIXELS
         
     #Food Handling
-    spawn_food(screen, food_pos_x, food_pos_y) 
-    distance_head_food = math.sqrt(pow(food_pos_x - player_pos_x, 2) + pow(food_pos_y - player_pos_y, 2))
+    spawn_food(screen, food_pos_x, food_pos_y)   
+    distance_head_food = math.sqrt(pow(food_pos_x - snake.player_pos_x, 2) + pow(food_pos_y - snake.player_pos_y, 2))
     if distance_head_food < PIXELS:
         score_value += 1
+        snake.snake_body.append(snake.snake_directional())
         draw_background(screen)
-        food_pos_x = random.randrange(0, WIDTH - PIXELS, 32)
-        food_pos_y = random.randrange(0, HEIGHT - PIXELS, 32)
+        food_pos_x , food_pos_y = get_food_pos(snake.snake_body)
+       
         spawn_food(screen, food_pos_x, food_pos_y)
 
     draw_scoreboard(screen, score_value, scoreboard_x_coord, scoreboard_y_coord)
 
     pygame.display.flip()
     
-    dt = clock.tick(60) / 1000
+    dt = clock.tick(30) / 1000
 
 pygame.quit()
---
+++ snake.py
@@ -0,0 +1,40 @@
+import pygame
+import math
+import random
+
+
+class Snake:
+    def __init__(self, name):
+        self.player_vel_x = 0
+        self.player_vel_y = 0
+        self.current_direction = " "
+        self.movement_speed = 400
+        self.player_pos_x = 240
+        self.player_pos_y = 240
+        self.snake_body = [[self.player_pos_x, self.player_pos_y],
+                           [208, self.player_pos_y],
+                           [176, self.player_pos_y]]
+        self.name=name
+        
+
+    def snake_update(self):
+        new_head = [self.player_pos_x, self.player_pos_y]
+        self.snake_body.insert(0, new_head)
+        self.snake_body.pop()
+    
+ 
+    def snake_draw(self, PIXELS, screen):
+        for i in self.snake_body:
+            pygame.draw.rect(screen, "purple", [(i), (PIXELS, PIXELS)])
+        
+            
+    def snake_directional(self):
+        if self.current_direction == 'd':
+            self.update_seg = [self.snake_body[-1][0] - 32, self.snake_body[-1][1]]
+        if self.current_direction == 'a':
+            self.update_seg = [self.snake_body[-1][0] + 32, self.snake_body[-1][1]]
+        if self.current_direction == 'w':
+            self.update_seg = [self.snake_body[-1][0], self.snake_body[-1][1] - 32]
+        if self.current_direction == 's':
+            self.update_seg = [self.snake_body[-1][0], self.snake_body[-1][1] + 32]
+        return self.update_seg
